f6afffb6f990de13a1def196be763cf54bb2a01c
Prachi Surya <prachi6mactosys@gmail.com>
2023-06-29 18:38:12 +0530

    I add the QRcode file

---
+++ reactqrwithant/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import QrCode from './components/QrCode';
+
+function App() {
+  return (
+    <div className="App">
+     <QrCode />
+    </div>
+  );
+}
+
+export default App;
